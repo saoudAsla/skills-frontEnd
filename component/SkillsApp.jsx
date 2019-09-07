1325a9e3b35ecbb0398d3e3b77118f49fc9dc3b1
saoudAsla <54233628+saoudAsla@users.noreply.github.com>
2019-09-07 23:23:56 +0300

    add folder: component, add file: skillApp.jsx, modified: app.js

---
+++ component/SkillsApp.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
+import SkillsOverTime from './SkillsOverTime';
+import LeftMenu from './LeftMenu';
+
+
+class SkillsApp extends Component {
+    render() {
+        return (
+            <Router>
+                <>
+          
+                    
+                    
+                
+                </>
+            </Router>
+        )
+    }
+}
+
+export default SkillsApp
