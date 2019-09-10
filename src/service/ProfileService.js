c21ab0e3dcee3b1e5e3853302704cac3721fc7da
AmjadNas <amjadn1092@gmail.com>
2019-09-10 14:50:53 +0300

    Update ProfileService.js

--- src/service/ProfileService.js
+++ src/service/ProfileService.js
@@ -7,15 +7,41 @@ const USERNAME = 'someUser';
 const BASE_API_URL = `http://localhost:8080/api`;
 
 class EmployeeDataService {
-  retrieveProfile(id) {
+ async retrieveProfile(id) {
+
+ /*  try {
+     const response = await fetch(`${BASE_API_URL}/employee/${id}`);
+   const data = await response.json();
+
+   const imageRes = await fetch(data.imageUrl);
+   // parse image
+
+   //return data;
+   } catch (error) {
+     throw error;  // handle error in UI
+   }*/
+
     return employee;
   }
 
-  retrieveProfilePicture(id) {
-    return employee;
+  async retrieveProfilePicture(id) {
+   /*  const imageRes = await fetch(`imageURI/${id}`);
+   // parse image*/
+   // return image;
   }
 
-  retrieveEmployeeAssignments(id) {
+  async retrieveEmployeeAssignments(id) {
+    /*  try {
+     const response = await fetch(`${BASE_API_URL}/assignments/${id}`);
+   const assignments = await response.json();
+
+   const imageRes = await fetch(data.imageUrl);
+   // parse image
+
+   //return data;
+   } catch (error) {
+     throw error; // handle error in UI
+   }*/
     return assignments;
   }
 }
