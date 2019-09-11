0eb790781f95754df787bca066bacee46f3c05fc
AmjadNas <amjadn1092@gmail.com>
2019-09-11 11:44:21 +0300

    Update LeftMenu.jsx

--- src/component/LeftMenu.jsx
+++ src/component/LeftMenu.jsx
@@ -4,10 +4,10 @@ import DataService from '../service/SkillsDataService';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 
 class LeftMenu extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      assignements:DataService.retrieveAssignementsOfEmployeeById(2),
+    this.state = {
+      assignements: DataService.retrieveAssignementsOfEmployeeById(2),
     }
   }
 
@@ -15,36 +15,36 @@ class LeftMenu extends Component {
     return (
 
       <div className="card bg-light">
-        
+
 
         <div class="card-body" style={{ margin: 40 }}>
           <img src={prof2} width='120' height='130' />
         </div>
 
         <ul class="list-group list-group-flush">
-          <li class="list-group-item"><Link to={{ pathname: '/home' }}>Own Skills</Link></li>
+          <li class="list-group-item"><Link to={{ pathname: '/home/1' }}>Own Skills</Link></li>
           <li class="list-group-item"><Link to={{ pathname: '/profile/1' }}>Profile</Link></li>
           <li class="list-group-item"><Link to={{ pathname: '/managerView' }}>Manager View</Link></li>
         </ul>
 
 
- 
- 
+
+
         <div class="card-body">
           <h5 class="card-title">News</h5>
           {
             this.state.assignements.map(
-              assignement=>
-              <p className="card-text">
-              <br/>Project:&nbsp;{assignement.projectName}
-              <br/>{assignement.startDate}&nbsp; To &nbsp;{assignement.endDate}
-              <br/><font color={assignement.status =="In Progress"?"green":"red"}>{assignement.status}</font>
-              </p>       
+              assignement =>
+                <p className="card-text">
+                  <br />Project:&nbsp;{assignement.projectName}
+                  <br />{assignement.startDate}&nbsp; To &nbsp;{assignement.endDate}
+                  <br /><font color={assignement.status == "In Progress" ? "green" : "red"}>{assignement.status}</font>
+                </p>
             )
           }
         </div>
- 
- 
+
+
 
       </div >
     )
